6750f33e286f05dba133ca2316f0a1dccf6be5a5
Julian Ruess <julianr@linux.ibm.com>
2024-07-02 17:48:11 +0200

    wezterm: window padding 0
    Signed-off-by: Julian Ruess <julianr@linux.ibm.com>

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,6 +6,12 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 config.window_decorations = "None"
 config.enable_tab_bar = false
 -- For example, changing the color scheme:
