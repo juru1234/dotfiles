52c891773b1ac107183d86f187021bb0e2082270
Julian Ruess <julianonline+github@posteo.de>
2024-07-02 20:58:20 +0200

    kitty: use wezterm regex
    Signed-off-by: Julian Ruess <julianonline+github@posteo.de>

--- .config/kitty/custom-hints.py
+++ .config/kitty/custom-hints.py
@@ -5,8 +5,36 @@ def mark(text, args, Mark, extra_cli_args, *a):
     # matching text. extra_cli_args are any extra arguments
     # passed on the command line when invoking the kitten.
     # We mark all individual word for potential selection
-    pattern = r"[/\.a-zA-Z0-9][/\-\_\.A-Za-z0-9]{3,}(:\d+)?"
-    pattern = ['(?:https?://|git@|git://|ssh://|ftp://|file:///)\S+', '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}']
+    pattern = [
+    ## markdown_url
+    '\[[^]]*\]\(([^)]+)\)',
+    ## url
+    '(?:https?:##|git@|git://|ssh://|ftp://|file:///)\S+',
+    ## diff_a
+    '--- a/(\S+)',
+    ## diff_b
+    '\+\+\+ b/(\S+)',
+    ## docker
+    'sha256:([0-9a-f]{64})',
+    ## path
+    '(?:[.\w\-@~]+)?(?:/[.\w\-@]+)+',
+    ## color
+    '#[0-9a-fA-F]{6}',
+    ## uuid
+    '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',
+    ## ipfs
+    'Qm[0-9a-zA-Z]{44}',
+    ## sha
+    '[0-9a-f]{7,40}',
+    ## ip
+    '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
+    ## ipv6
+    '[A-f0-9:]+:+[A-f0-9:]+[%\w\d]+',
+    ## address
+    '0x[0-9a-fA-F]+',
+    ## number
+    '[0-9]{4,}',
+    ]
     regex = re.compile(r'\b(' + '|'.join(pattern) + r')\b')
 
     for idx, m in enumerate(re.finditer(regex, text)):
