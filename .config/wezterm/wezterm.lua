f7b2cb33fd37de7270339bf5650964624779c185
Julian Ruess <julianr@linux.ibm.com>
2024-07-02 17:27:20 +0200

    wezterm: change config
    Signed-off-by: Julian Ruess <julianr@linux.ibm.com>

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,66 +6,10 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.window_decorations = "None"
+config.enable_tab_bar = false
 -- For example, changing the color scheme:
 config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.font_size = 12
 
-config.unix_domains = {
-  {
-    name = 'example',
-    proxy_command = { "ssh", "-T", "-A", "example", "wezterm", "cli", "proxy" },
-  },
-}
-
-config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
-config.keys = {
-  {
-    key = '%',
-    mods = 'LEADER|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = '"',
-    mods = 'LEADER|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-  {
-    key = 'a',
-    mods = 'LEADER|CTRL',
-    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
-  },
-{
-    key = "h",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Down"),
-  },
-  {
-    key = "k",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Up"),
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-  {
-    key = "-",
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-}
-
-for i = 1, 8 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'LEADER',
-    action = wezterm.action.ActivateTab(i - 1),
-  })
-end
+return config
