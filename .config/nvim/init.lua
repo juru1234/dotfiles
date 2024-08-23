cf8b13cd8941050684ec3b7199ae8e18b7554fef
Julian Ruess <julianr@linux.ibm.com>
2024-08-23 10:05:09 +0200

    nvim: move plugins in separate file
    Signed-off-by: Julian Ruess <julianr@linux.ibm.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,34 +1,5 @@
--- Automatically install vimplug
-local data_dir = vim.fn.stdpath('data')
-if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
-	vim.cmd('silent !curl -fLo ' ..
-		data_dir ..
-		'/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
-	vim.o.runtimepath = vim.o.runtimepath
-	vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
-end
-
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin')
--- vim plugins
-Plug 'tpope/vim-fugitive'
-Plug 'tpope/vim-obsession'
-Plug 'airblade/vim-gitgutter'
-Plug 'takac/vim-hardtime'
-Plug 'morhetz/gruvbox'
--- neovim plugins
-Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'j-hui/fidget.nvim'
-Plug 'nvim-lualine/lualine.nvim'
-Plug('ibhagwan/fzf-lua', { branch = 'main' })
-Plug 'ggandor/leap.nvim'
-Plug 'stevearc/oil.nvim'
-vim.call('plug#end')
-
 require('helpers')
+require('plugins')
 require('keymaps')
 require('lsp')
 
