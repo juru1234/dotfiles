6c1359c0347b83c0ebfe4e806df911bca3053dd9
Julian Ruess <julianr@linux.ibm.com>
2024-10-23 14:46:24 +0200

    nvim: add helper to act as a pager
    Signed-off-by: Julian Ruess <julianr@linux.ibm.com>

--- .config/nvim/lua/helpers.lua
+++ .config/nvim/lua/helpers.lua
@@ -33,3 +33,13 @@ vim.api.nvim_create_user_command(
 	end,
 	{ nargs = '?' }
 )
+
+-- helper to act as a pager
+-- git grep --color=always foo | nvim +Term
+vim.api.nvim_create_user_command("Term", function(args)
+    local buf = vim.api.nvim_get_current_buf()
+    local b = vim.api.nvim_create_buf(false, true)
+    local chan = vim.api.nvim_open_term(b, {})
+    vim.api.nvim_chan_send(chan, table.concat(vim.api.nvim_buf_get_lines(buf, 0, -1, false), "\n"))
+    vim.api.nvim_win_set_buf(0, b)
+end, {})
