e2cbc67a996fc3fbd1110a900c6750e235a199be
Julian Ruess <julianr@linux.ibm.com>
2024-11-19 11:08:45 +0100

    nvim: also handle multiple >
    Signed-off-by: Julian Ruess <julianr@linux.ibm.com>

--- .config/nvim/lua/helpers.lua
+++ .config/nvim/lua/helpers.lua
@@ -37,11 +37,11 @@ vim.api.nvim_create_user_command(
 -- helper to act as a pager
 -- e.g. git grep --color=always foo | nvim +Pager
 vim.api.nvim_create_user_command("Pager", function(args)
-    local buf = vim.api.nvim_get_current_buf()
-    local b = vim.api.nvim_create_buf(false, true)
-    local chan = vim.api.nvim_open_term(b, {})
-    vim.api.nvim_chan_send(chan, table.concat(vim.api.nvim_buf_get_lines(buf, 0, -1, false), "\n"))
-    vim.api.nvim_win_set_buf(0, b)
+	local buf = vim.api.nvim_get_current_buf()
+	local b = vim.api.nvim_create_buf(false, true)
+	local chan = vim.api.nvim_open_term(b, {})
+	vim.api.nvim_chan_send(chan, table.concat(vim.api.nvim_buf_get_lines(buf, 0, -1, false), "\n"))
+	vim.api.nvim_win_set_buf(0, b)
 end, {})
 
 -- helper to use nvim for composing mails
@@ -57,12 +57,12 @@ vim.api.nvim_create_user_command("EditMail", function(args)
 	vim.api.nvim_win_set_buf(0, b)
 	vim.cmd('setlocal modifiable')
 
-	-- Handle >
+	-- Handle one or multiple >
 	vim.api.nvim_set_hl(0, 'quotedDeletion', { fg = 'red' })
 	vim.api.nvim_set_hl(0, 'quotedAddition', { fg = 'green' })
 	vim.api.nvim_set_hl(0, 'quote', { fg = 'white' })
 
-	vim.api.nvim_command('syntax match quote "^>* "')
-	vim.api.nvim_command('syntax match quotedDeletion "^>* *-.*" contains=quote')
-	vim.api.nvim_command('syntax match quotedAddition "^>* *+.*" contains=quote')
+	vim.api.nvim_command('syntax match quote "^>\\( \\?>\\)* "')
+	vim.api.nvim_command('syntax match quotedDeletion "^>\\( \\?>\\)* *-.*" contains=quote')
+	vim.api.nvim_command('syntax match quotedAddition "^>\\( \\?>\\)* *+.*" contains=quote')
 end, {})
