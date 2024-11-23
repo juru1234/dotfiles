da820f3002517e959c6d78dbf30fc79b173146a3
Julian Ruess <julianonline+github@posteo.de>
2024-11-24 00:52:54 +0100

    nvim: this config needs neovim 0.11
    Signed-off-by: Julian Ruess <julianonline+github@posteo.de>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
-if vim.fn.has("nvim-0.10") == 0 then
-    vim.notify("This config only supports Neovim 0.10+", vim.log.levels.ERROR)
+if vim.fn.has("nvim-0.11") == 0 then
+    vim.notify("This config only supports Neovim 0.11+", vim.log.levels.ERROR)
     return
 end
 
