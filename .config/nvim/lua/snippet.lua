4c1e968f40837a502d4c4a598a9a9e48c4ae5505
Julian Ruess <julianonline+github@posteo.de>
2024-11-24 00:45:06 +0100

    nvim: add 2 user defined snippets
    Signed-off-by: Julian Ruess <julianonline+github@posteo.de>

--- .config/nvim/lua/snippet.lua
+++ .config/nvim/lua/snippet.lua
@@ -10,3 +10,15 @@ function vim.snippet.add(trigger, body, opts)
         vim.snippet.expand(body)
     end, opts)
 end
+
+
+vim.snippet.add(
+    "fn",
+    "function ${1:name}($2)\n\t${3:-- content}\nend",
+    { buffer = 0 }
+)
+vim.snippet.add(
+    "lfn",
+    "local function ${1:name}($2)\n\t${3:-- content}\nend",
+    { buffer = 0 }
+)
