ba4d3a9bb3e7713036c7c708460200a3c91d8a80
Julian Ruess <julianr@linux.ibm.com>
2025-11-07 10:35:04 +0100

    nvim: keymap conform to space+f
    Signed-off-by: Julian Ruess <julianr@linux.ibm.com>

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -75,9 +75,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'rn', vim.lsp.buf.rename, opts)
         vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
+        vim.keymap.set("n", "<space>f", function()
+            require("conform").format({ async = true, lsp_fallback = true })
+            end, { desc = "Format Buffer" })
+
+        --vim.keymap.set('n', '<space>f', conform.format())
         vim.keymap.set('v', '<space>f', function()
             vim.lsp.buf.format { async = true }
         end, opts)
